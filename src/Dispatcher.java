634fbaa862f2a51a888df866ee73b6d49ef1ecd6
Mengwei Ding <mengwei.ding@gmail.com>
2013-03-02 02:24:22 -0500

    debug variable

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -1,4 +1,17 @@
+import java.util.*;
 
-public class Dispatcher {
+public class Dispatcher implements Runnable {
+  // the referenced objects table in this remote server
+  private Map<String, Object> objectTable;
+  
+  public static final int port = 55555;
+  
+  public Dispatcher() {
+    this.objectTable = new TreeMap<String, Object>();
+  }
 
+  @Override
+  public void run() {
+    
+  }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 
 public class Main {
+  
+  public static final boolean DEBUG = true;;
 
   public static void main(String[] args) {
 
