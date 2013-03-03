f2f6e70932d1b9f8c4f2846a6a2bc16ff598bdad
Mengwei Ding <mengwei.ding@gmail.com>
2013-03-03 18:23:18 -0500

    add some log information

--- src/TestClient.java
+++ src/TestClient.java
@@ -9,19 +9,23 @@ public class TestClient {
     String rip = args[0];
     int rport = Integer.parseInt(args[1]);
 
+    System.out.println("Request a remote object \"testobj\" in terms of interface \"TestInterface1\" ");
     TestInterface1 testobj1 = (TestInterface1) StubCompiler.compile("testobj",
             TestInterface1.class, rip, rport);
     if (testobj1 != null) {
+      System.out.println("Call add(10, 9) of object \"testobj\", waiting for result...");
       int sum = testobj1.add(10, 9);
-      System.out.println("Sum = " + sum);
+      System.out.println("Receive result: " + sum);
     }
 
+    System.out.println("Request a remote object \"testobj\" in terms of interface \"TestInterface2\" ");
     TestInterface2 testobj2 = (TestInterface2) StubCompiler.compile("testobj",
             TestInterface2.class, rip, rport);
     if (testobj2 != null) {
       Integer[] elements = { 1, 2, 3, 4, 5, 6, 7, 8 };
+      System.out.println("Call addAll(int[]{ 1, 2, 3, 4, 5, 6, 7, 8 }) of object \"testobj\", waiting for result...");
       int sum = testobj2.addAll(elements);
-      System.out.println("Sum all = " + sum);
+      System.out.println("Receive result: " + sum);
     }
   }
 
