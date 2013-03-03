98e0673d782c86d3b771dba4a85ab68ac1bf874a
Mengwei Ding <mengwei.ding@gmail.com>
2013-03-03 18:29:23 -0500

    add some log message

--- src/DispatcherMessageHandler.java
+++ src/DispatcherMessageHandler.java
@@ -7,12 +7,12 @@ import java.net.Socket;
  * The class handles each remote method invocation call request.
  */
 public class DispatcherMessageHandler extends MessageHandler {
-  
+
   public Dispatcher dispatcher;
-  
+
   public DispatcherMessageHandler(Socket s, Dispatcher dp) {
     super(s);
-    
+
     this.dispatcher = dp;
   }
 
@@ -20,25 +20,25 @@ public class DispatcherMessageHandler extends MessageHandler {
   public void handle() {
     System.out.println("Handle this InvocationRequestMessage.");
     Object rawmsg = this.getMessage();
-    
+
     if (rawmsg.getClass().getName().compareTo(InvocationRequestMessage.class.getName()) != 0) {
       // invalid message
       System.err.println("Invalid Message");
-      return ;
+      return;
     }
-    
+
     InvocationRequestMessage msg = (InvocationRequestMessage) rawmsg;
-    
+
     // get the actual reference of the request object
     Object refObj = this.dispatcher.getReference(msg.getRefObjId());
-    
+
     Object returnValue = null;
-    
+
     if (refObj != null) {
       try {
         // get the class of the request interface
         Class<?> interfaceClass = msg.getInterfaceClass();
-        
+
         // get the arguments for the method invocation
         Object[] args = msg.getArguments();
 
@@ -47,22 +47,23 @@ public class DispatcherMessageHandler extends MessageHandler {
         for (int i = 0; i < args.length; i++) {
           argsclasses[i] = args[i].getClass();
         }
-        
+
         // invoke this method
         Method method = interfaceClass.getMethod(msg.getMethodStr(), argsclasses);
-        System.out.println("Invoke the requested method.");
+        System.out.println("Invoke the requested method " + interfaceClass.getName() + "."
+                + method.getName() + "() with " + args.length + " arguments.");
         returnValue = method.invoke(refObj, args);
       } catch (Exception e) {
         // if the method call throw a exception, catch it here;
         returnValue = new RemoteException(e.getMessage());
       }
-    }else {
+    } else {
       returnValue = new RemoteException("Object not found.");
     }
-    
+
     // patch the return value into the response message
     InvocationResponseMessage resmsg = new InvocationResponseMessage(returnValue);
-    
+
     try {
       // send back the response message
       System.out.println("Send back the result by an InvocationResponseMessage.\n");
